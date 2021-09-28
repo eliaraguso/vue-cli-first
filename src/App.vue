74ff2d207558ff6811b3c4bf1fa0595016051878
eliaraguso <elia.raguso@gmail.com>
2021-09-28 16:55:36 +0200

    Inserito un componente titolo nell'app Vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,19 @@
 <template>
   <div id="app">
+    <Title></Title>
+
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Title from './components/Title.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Title
   }
 }
 </script>
